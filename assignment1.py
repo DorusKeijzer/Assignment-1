0d71739fc7395130cf1694130393580df5320925
tijn47 <159795335+tijn47@users.noreply.github.com>
2024-02-12 16:26:50 +0100

    Update assignment1.py met automatische intersections

--- assignment1.py
+++ assignment1.py
@@ -48,3 +48,29 @@ if __name__=="__main__":
 
         # close the window 
         cv.destroyAllWindows() 
+
+
+#code door tijn voor automatisch tekenen van hoekpunten
+# Import images from the image folder
+images = glob.glob('images/IMG_4049.jpg')
+
+# termination criteria
+criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+objp = np.zeros((6*9,3), np.float32)
+objp[:,:2] = np.mgrid[0:9,0:6].T.reshape(-1,2)
+# Arrays to store object points and image points from all the images.
+objpoints = [] # 3d point in real world space
+imgpoints = [] # 2d points in image plane.
+
+
+for fname in images:
+    img = cv.imread(fname)
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    # Find the chess board corners
+    ret, corners = cv.findChessboardCorners(gray, (6,9), None)
+    # Draw and display the corners
+    cv.drawChessboardCorners(img, (6,9), corners, ret)
+    cv.imshow('img', img)
+    cv.waitKey(500)
+cv.destroyAllWindows()
