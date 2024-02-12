929e8b66ae2d070c38e4076ab593b743d1949b5d
Dorus Keijzer <83274869+DorusKeijzer@users.noreply.github.com>
2024-02-12 15:54:02 +0100

    created click event

--- assignment1.py
+++ assignment1.py
@@ -2,5 +2,15 @@ import cv2 as cv
 import numpy as np
 import glob
 
-# Import images from the image folder
-images = glob.glob('Images/*.jpg')
+# Gets the filenames of the images in the Images directory
+images = glob.glob('images/*.jpg')
+
+for filename in images:
+    img = cv.imread(filename)
+
+def click_event(event, x, y, flags, params): 
+    if event == cv.EVENT_FLAG_LBUTTON:
+        corner = x,y
+        yield corner
+    
+
