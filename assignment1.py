1aff80bd22b301262ba6ec31ff2abb7ac6dc973f
Dorus Keijzer <83274869+DorusKeijzer@users.noreply.github.com>
2024-02-12 16:07:03 +0100

    Implemented mouse click event for corners

--- assignment1.py
+++ assignment1.py
@@ -8,9 +8,34 @@ images = glob.glob('images/*.jpg')
 for filename in images:
     img = cv.imread(filename)
 
+corners = []
+
 def click_event(event, x, y, flags, params): 
     if event == cv.EVENT_FLAG_LBUTTON:
-        corner = x,y
-        yield corner
-    
+        corners.append((x,y))
 
+if __name__=="__main__":   
+    # reading the image 
+    img = np.zeros((500, 500, 3), np.uint8)
+    cv.namedWindow('image')
+    cv.setMouseCallback('image', click_event) 
+
+    while(True):
+        # displaying the image 
+        cv.imshow('image', img) 
+    
+        # setting mouse handler for the image 
+        # and calling the click_event() function 
+    
+        if len(corners) == 4:
+            print(corners)
+            break
+        # wait for a key to be pressed to exit 
+        k = cv.waitKey(1) & 0xFF
+ 
+        if k == 27:
+            break
+
+
+    # close the window 
+    cv.destroyAllWindows() 
