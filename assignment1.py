92cef89d16b8e29df220588d52675a9393d02cfc
Dorus Keijzer <83274869+DorusKeijzer@users.noreply.github.com>
2024-02-13 14:52:29 +0100

    Used the built in function for perspective transformation

--- assignment1.py
+++ assignment1.py
@@ -51,72 +51,10 @@ def manualCorners(img, chessboardwidth, chessboardheight) -> np.array:
     clickcorners = []
     cv.destroyAllWindows()
  
-    interpolation = interpolateCorners(res, chessboardwidth, chessboardheight)
+    interpolation = interpolate(res, chessboardwidth, chessboardheight)
 
     return interpolation
 
-def homogenize(vector):
-    """Returns the homogeneous vector given an input vector"""
-    return np.hstack((vector, np.ones((vector.shape[0], 1))))
-
-def heterogenize(vector):
-    return vector[:, :-1]
-
-
-def fitAffine(source, destination):
-    """ When given two sets of input vectors of corresponding size, 
-        returns the affine transformation that moves source points to destination"""
-    # makes homogeneous coordinates out of source coordinates by appending a 1 to each coordinate
-    homo_source = homogenize(source)
-
-    # Fits these coordinates
-    T, _ = np.linalg.lstsq(homo_source, destination, rcond=None)[0:2]
-
-    # this yields a 3 x 2 transformation matrix. 
-    # We need to append [0,0,1].T to turn this into the full transformation matrix
-    new_column = np.array([[0], [0], [1]])
-
-    # Concatenate the new column to the original array
-    Transformation = np.hstack((T, new_column))
-
-    return Transformation.T
-
-    
-    #
-
-
-
-def interpolateCorners(corners, chessboardwidth, chessboardheight) -> np.array:
-    """Expects:
-       the pixel coordinates of the 4 corners of a chessboard in an np array of size 4
-       the proportions of the chessboard, 
-
-       returns where the internal crossings are as a np array of (chessboardwidth x chessboard height) """
-    
-    # the coordinates of the outer corners of the chessboard in chessboard coordinates
-    # these will be used to solve for the transformation matrix
-    PreTransformCorners = np.array([[0,0], 
-                                   [0,chessboardwidth-1], 
-                                   [chessboardheight-1, 0], 
-                                   [chessboardheight-1, chessboardwidth-1]])    
-    
-    # Finds the transformation matrix using least squares
-    Transformation = fitAffine(PreTransformCorners, corners)
-
-    # The coordinates of the chessboard corners in chessboard coordinates (i.e. [[[0,0],[1,0],[2,0],...
-    #                                                                            [[1,1],[1,2],[1,3],..)
-    # these will be transformed to yield the image coordinates of the corners
-    chessboardCoords = np.mgrid[0:chessboardheight,0:chessboardwidth].T.reshape(-1,2)
-    chessboardCoords = homogenize(chessboardCoords)
-    
-
-    # Performs the transformation on the chessboard points
-    PostTransformationChessboard = np.dot(Transformation, chessboardCoords.T).T
-    
-    # returns to non homogenous coordinates
-    PostTransformationChessboard = heterogenize(PostTransformationChessboard)
-    return PostTransformationChessboard.reshape((54, 1, 2))
-    
 def resizeImage(img, resize_width):
     """Resizes the image so that the image is of the specified width"""
     height, width = img.shape[:2]
@@ -132,7 +70,30 @@ def prepareImage(filename):
     cv.namedWindow('image')
     return img
 
+def interpolate(corners, chessboardwidth, chessboardheight):
+    """Expects:
+       the pixel coordinates of the 4 corners of a chessboard in an np array of size 4
+       the proportions of the chessboard, 
 
+       returns where the internal crossings are as a np array of (chessboardwidth x chessboard height) """
+
+    # the coordinates of the outer corners of the chessboard in chessboard coordinates
+    # these will be used to solve for the transformation matrix
+    corner_chesscoordinates = np.float32([[0,0], 
+                         [0,chessboardwidth-1], 
+                         [chessboardheight-1, 0], 
+                         [chessboardheight-1, chessboardwidth-1]])   
+    
+    # Finds the transformation matrix 
+    TransformationMatrix = cv.getPerspectiveTransform(corners, corner_chesscoordinates)
+
+    # The coordinates of the chessboard corners in chessboard coordinates (i.e. [[[0,0]],[[0,1]],[[0,2]],...
+    #                                                                            [[1,1]],[[1,2]],[[1,3]],..)
+    chessboard_coords = np.float32([[[x, y]] for x in range(chessboardwidth) for y in range(chessboardheight)])
+
+    # applies the transformation
+    inner_corners = cv.perspectiveTransform(chessboard_coords, TransformationMatrix)
+    return inner_corners.reshape(-1, 1, 2)
 
 if __name__=="__main__":   
     # reading the image 
@@ -144,23 +105,21 @@ if __name__=="__main__":
         # Find the chess board corners
         ret, corners = cv.findChessboardCorners(img, (CHESSBOARDWIDTH,CHESSBOARDHEIGHT), None)
         # If the built in function cannot distinguish the corner points, the user must manually mark the corners
-        if not ret:
+        if ret: 
+            corners2 = cv.cornerSubPix(img, corners, (11,11), (-1,-1), criteria)
+        else:
             cv.setMouseCallback('image', click_event) 
             corners = manualCorners(img, CHESSBOARDWIDTH, CHESSBOARDHEIGHT)
             ret = True
 
         objpoints.append(objp)
-        try: 
-            corners2 = cv.cornerSubPix(img, corners, (11,11), (-1,-1), criteria)
-        except:
-            corners2 = corners
-        print(corners2)
+        print(corners2.shape)
         imgpoints.append(corners2)
 
         # Draw and display the corners
         cv.drawChessboardCorners(img, (CHESSBOARDHEIGHT,CHESSBOARDWIDTH), corners2, ret)
         cv.imshow('Image', img)
-        cv.waitKey(5000)
+        cv.waitKey()
 
         # close the window 
         cv.destroyAllWindows() 
