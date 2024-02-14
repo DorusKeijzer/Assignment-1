c9738ea509c52028a18a901ac70be20d99e7840a
Dorus Keijzer <83274869+DorusKeijzer@users.noreply.github.com>
2024-02-14 12:46:41 +0100

    Created online webcam

---
+++ OnlineWebcam.py
@@ -0,0 +1,32 @@
+import cv2 as cv
+import numpy as np
+
+cam = cv.VideoCapture(0)
+def draw(img, corners, imgpts):
+    # Extracting corner coordinates properly
+    corner = tuple(corners[0].ravel())
+    corner = tuple(map(int, corner))
+    # Extracting imgpts coordinates properly
+    imgpts = np.int32(imgpts).reshape(-1, 2)
+    imgpts = imgpts.astype(int)  # Convert imgpts to Python integers
+    
+    # Drawing lines from corner to imgpts        
+    img = cv.line(img, corner, tuple(imgpts[0]), (255, 0, 0), 2)
+    img = cv.line(img, corner, tuple(imgpts[1]), (0, 255, 0), 2)
+    img = cv.line(img, corner, tuple(imgpts[2]), (0, 0, 255), 2)
+
+    return img
+
+while True:
+    check, frame = cam.read()
+
+    gray = cv.cvtColor(frame,cv.COLOR_BGR2GRAY)
+    ret, corners = cv.findChessboardCorners(gray, (6,9),None)
+    cv.imshow('video', frame)
+
+    key = cv.waitKey(1)
+    if key == 27:
+        break
+
+cam.release()
+cv.destroyAllWindows()
